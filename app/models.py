aecc2f8af694df9763f58554ef05d598ca1160a4
Zach <zfred09@gmail.com>
2018-02-12 06:13:22 -0600

    Databases defined

--- app/models.py
+++ app/models.py
@@ -1,18 +1,25 @@
 from datetime import datetime
 from app import db
 
-class User(db.Model):
+class Recipes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_has = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    title = db.Column(db.String(120), index=True)
+    prep_time = db.Column(db.Integer)
+    cook_time_min = db.Column(db.Integer)
 
+    # backref sets the virtual field name for Ingredients' recipe_id field
+    ingredients = db.relationship('Ingredients', backref='ingredients', lazy='dynamic')
+
+    # Tells Python how to print objects of this class - for debugging
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<Recipes {}>'.format(self.title)
 
-class Post(db.Model):
+class Ingredients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    ingredient = db.Column(db.String(70))
+    quantity = db.Column(db.String(15))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+
+    # Tells Python how to print objects of this class - for debugging
+    def __repr__(self):
+        return '<Ingredients {} - {} - {}>'.format(self.ingredient, self.quantity, self.recipe_id)
